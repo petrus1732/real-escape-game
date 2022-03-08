d55c867a01e97e4cec3f6324d297e6cc40f9e2b5
petrus1732 <91786464+petrus1732@users.noreply.github.com>
2022-03-08 23:53:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const goToSecondPage = () => {
+    window.location = './second_page.html';
+    console.log('QQ');
+}
+
+const goToThirdPage = () => {
+    window.location = './third_page.html';
+    console.log('QQ');
+}
+
+let inputField = '';
+let idx = 1;
+let answer = '6537';
+
+const password = (e) => {
+    if (idx<=4) {
+        document.getElementById('d'+idx).innerText = e.innerText;
+        idx++;
+    }
+}
+
+const del = () => {
+    if (idx>1) {
+        idx--;
+        document.getElementById('d'+idx).innerText = '';
+    }
+}
+
+const clr = () => {
+    for (let i=1; i<=4; i++){
+        document.getElementById('d'+i).innerText = '';
+    }
+    idx=1;
+}
+
+const checkAnswer = () => {
+    let submission = '';
+    for (let i = 1; i<=4; i++) {
+        submission += document.getElementById('d'+i).innerText;
+    }
+    if (idx === 5) {
+        if (submission === answer) {
+            document.getElementById('instruction').innerHTML = 'Yeah, you got it';
+            for (let i = 1; i<=4; i++) {
+                document.getElementById('d'+i).style.animation = "changeToGreen 1s forwards";
+            }
+        }
+        else {
+            document.getElementById('instruction').innerHTML = "Woops, it's worng";
+        }
+    }
+    else {
+        document.getElementById('instruction').innerHTML = '輸入必須為四位數字!';
+    }
+}
